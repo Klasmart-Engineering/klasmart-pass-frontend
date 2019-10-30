a33a48fecf72c83315278ca1f18d2162cb838cf6
owen-delahoy <owen.delahoy@calmid.com>
2019-10-30 14:46:35 +0900

    Validation and response changes

--- server/src/main.ts
+++ server/src/main.ts
@@ -34,8 +34,8 @@ function main() {
 
     app.get("/token", async (req: Request, res: Response) => {
         try {
-            const token = await gateway.clientToken.generate({});
-            res.json({token});
+            const {clientToken} = await gateway.clientToken.generate({});
+            res.json({clientToken});
         } catch (e) {
             console.error(e);
             res.send({error: true});
@@ -45,8 +45,14 @@ function main() {
     app.get("/payment/:id", async (req: Request, res: Response) => {
         try {
             const transactionId = req.params.id;
+            if (!transactionId) {
+                res.statusCode = 400;
+                res.send({error: true});
+                return;
+            }
+
             const transaction = await gateway.transaction.find(transactionId);
-            res.json({transaction});
+            res.json(transaction);
         } catch (e) {
             console.error(e);
             res.send({error: true});
@@ -56,16 +62,22 @@ function main() {
     app.post("/payment", async (req: Request, res: Response) => {
         try {
             const paymentMethodNonce = req.body.payment_method_nonce;
+            if (!paymentMethodNonce) {
+                res.statusCode = 400;
+                res.send({error: true});
+                return;
+            }
+
             const {clientToken, transaction} = await gateway.transaction.sale({
                 amount: "1337",
                 options: {
-
                     storeInVaultOnSuccess: true,
                     submitForSettlement: true,
                 },
                 paymentMethodNonce,
             });
-            res.json({clientToken, transaction});
+            const transactionId = transaction.id;
+            res.json({clientToken, transactionId});
         } catch (e) {
             console.error(e);
             res.send({error: true});
@@ -75,3 +87,4 @@ function main() {
     const port = process.env.PORT || 8000;
     app.listen(port, () => console.log(`Listening on port ${port}`));
 }
+main();
