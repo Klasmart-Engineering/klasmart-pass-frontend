efbd181b3528ae448f4e8e9f1cc588448333b309
owen-delahoy <owen.delahoy@calmid.com>
2019-11-06 14:44:50 +0900

    Use react hooks to access ReactRouter v5 history

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,70 +1,194 @@
-import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper/Paper";
+import AppBar from "@material-ui/core/AppBar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Divider from "@material-ui/core/Divider";
+import Drawer from "@material-ui/core/Drawer";
+import IconButton from "@material-ui/core/IconButton";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import EmailIcon from "@material-ui/icons/Email";
+import LockOpenIcon from "@material-ui/icons/LockOpen";
+import MenuIcon from "@material-ui/icons/Menu";
+import PaymentIcon from "@material-ui/icons/Payment";
+import clsx from "clsx";
+import { useState } from "react";
 import * as React from "react";
-import { FormattedMessage, RawIntlProvider } from "react-intl";
-import { NavLink, Route, Switch } from "react-router-dom";
-import { getIntl } from "./locale/locale";
+import { FormattedMessage } from "react-intl";
+import { Route, Switch, useHistory } from "react-router-dom";
 import Login from "./pages/login";
-import {Payment} from "./pages/payment";
+import { Payment } from "./pages/payment";
 import SignUp from "./pages/signup";
 import Verify from "./pages/verify";
 
-interface Props { }
-interface State {
-    intl: ReturnType<typeof getIntl>;
-}
+const drawerWidth = 240;
 
-export class App extends React.Component<Props, State> {
-    constructor(props: Props) {
-        super(props);
-        const locales = [getIntl("ko"), getIntl("en")];
-        this.state = { intl: locales[1] };
-        this.previewLocalization(locales, 3000);
+// tslint:disable:object-literal-sort-keys
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            display: "flex",
+        },
+        appBar: {
+            transition: theme.transitions.create(["margin", "width"], {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.leavingScreen,
+            }),
+        },
+        appBarShift: {
+            width: `calc(100% - ${drawerWidth}px)`,
+            marginLeft: drawerWidth,
+            transition: theme.transitions.create(["margin", "width"], {
+                easing: theme.transitions.easing.easeOut,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+        },
+        menuButton: {
+            marginRight: theme.spacing(2),
+        },
+        hide: {
+            display: "none",
+        },
+        drawer: {
+            width: drawerWidth,
+            flexShrink: 0,
+        },
+        drawerPaper: {
+            width: drawerWidth,
+        },
+        drawerHeader: {
+            display: "flex",
+            alignItems: "center",
+            padding: theme.spacing(0, 1),
+            ...theme.mixins.toolbar,
+            justifyContent: "flex-end",
+        },
+        content: {
+            flexGrow: 1,
+            padding: theme.spacing(3),
+            transition: theme.transitions.create("margin", {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.leavingScreen,
+            }),
+            marginLeft: 0,
+        },
+        contentShift: {
+            transition: theme.transitions.create("margin", {
+                easing: theme.transitions.easing.easeOut,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+            marginRight: -drawerWidth,
+        },
+    }),
+);
+// tslint:enable:object-literal-sort-keys
+
+export function App() {
+    const classes = useStyles();
+    const [open, setOpen] = useState(false);
+    const history = useHistory();
+    function navigate(path: string) {
+        if (open) { setOpen(false); }
+        history.push(path);
     }
 
-    public render() {
-        return (
-            <RawIntlProvider value={this.state.intl}>
-                <Paper square>
-                    <Grid container spacing={5}>
-                        <Grid item xs={2}>
-                            <NavLink to="/" style={{ color: "inherit" }}>
-                                <FormattedMessage id="sign_up" />
-                            </NavLink>
-                        </Grid>
-                        <Grid item >
-                            <NavLink to="/login" style={{ color: "inherit" }}>
-                                <FormattedMessage id="login" />
-                            </NavLink>
-                        </Grid>
-                        <Grid item >
-                            <NavLink to="/payment" style={{ color: "inherit" }}>
-                                <FormattedMessage id="payment" />
-                            </NavLink>
-                        </Grid>
-                        <Grid item >
-                            <NavLink to="/verify" style={{ color: "inherit" }}>
-                                <FormattedMessage id="verify_email" />
-                            </NavLink>
-                        </Grid>
-                    </Grid>
-                </Paper>
+    return (
+        <div >
+            <CssBaseline />
+            <AppBar
+                position="fixed"
+                className={clsx(classes.appBar, { [classes.appBarShift]: open })}
+            >
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={() => setOpen(true)}
+                        edge="start"
+                        className={clsx(classes.menuButton, open && classes.hide)}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" noWrap>
+                        Badanamu
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                className={classes.drawer}
+                variant="persistent"
+                anchor="left"
+                open={open}
+                classes={{
+                    paper: classes.drawerPaper,
+                }}
+            >
+                <div className={classes.drawerHeader}>
+                    <IconButton onClick={() => setOpen(false)}>
+                        <ChevronLeftIcon />
+                    </IconButton>
+                </div>
+                <Divider />
+                <List>
+                    <ListItem
+                        button
+                        selected={history.location.pathname === "/"}
+                        onClick={() => navigate("/")}
+                    >
+                        <ListItemIcon>
+                            <AccountCircleIcon />
+                        </ListItemIcon>
+                        <ListItemText primary={<FormattedMessage id="sign_up"/>} />
+                    </ListItem>
+                    <ListItem
+                        button
+                        selected={history.location.pathname === "/login"}
+                        onClick={() => navigate("/login")}
+                    >
+                        <ListItemIcon>
+                            <LockOpenIcon />
+                        </ListItemIcon>
+                        <ListItemText primary={<FormattedMessage id="login"/>} />
+                    </ListItem>
+                    <ListItem
+                        button
+                        selected={history.location.pathname === "/payment"}
+                        onClick={() => navigate("/payment")}
+                    >
+                        <ListItemIcon>
+                            <PaymentIcon />
+                        </ListItemIcon>
+                        <ListItemText primary={<FormattedMessage id="payment"/>} />
+                    </ListItem>
+                    <ListItem
+                        button
+                        selected={history.location.pathname === "/verify"}
+                        onClick={() => navigate("/verify")}
+                    >
+                        <ListItemIcon>
+                            <EmailIcon />
+                        </ListItemIcon>
+                        <ListItemText primary={<FormattedMessage id="verify_email"/>} />
+                    </ListItem>
+                </List>
+                <Divider />
+            </Drawer>
+            <main className={clsx(classes.content, {[classes.contentShift]: open})}>
+                <div className={classes.drawerHeader} />
                 <Switch>
                     <Route path="/login" component={Login} />
                     <Route path="/payment" component={Payment} />
                     <Route path="/verify" component={Verify} />
                     <Route path="/" component={SignUp} />
                 </Switch>
-            </RawIntlProvider>
-                );
-            }
+            </main>
+        </div>
+    );
+}
 
-    private previewLocalization(locales: Array<ReturnType<typeof getIntl>>, interval = 5000) {
-                    let index = 0;
-                    setInterval(() => {
-                        index = (index + 1) % locales.length;
-                        const intl = locales[index];
-                        this.setState({intl});
-                }, interval);
-            }
-        }
+export default App;
--- client/src/client-entry.tsx
+++ client/src/client-entry.tsx
@@ -1,12 +1,23 @@
-import * as React from "react";
+import React, { useState } from "react";
 import * as ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import {App} from "./app";
+import { RawIntlProvider } from "react-intl";
+import { BrowserRouter, HashRouter } from "react-router-dom";
+import App from "./app";
+import { getIntl } from "./locale/locale";
 
 const div = document.getElementById("app");
 
-ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
-div);
+const locales = [getIntl("ko"), getIntl("en")];
+function ClientSide() {
+    const [index, setIndex] = useState(0);
+    setTimeout(() => setIndex((index + 1) % locales.length), 3000);
+    return (
+        <HashRouter>
+            <RawIntlProvider value={locales[index]}>
+                <App />
+            </RawIntlProvider>
+        </HashRouter>
+    );
+}
+
+ReactDOM.render(<ClientSide />, div);
