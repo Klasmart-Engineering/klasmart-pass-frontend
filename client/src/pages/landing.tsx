3de248e0b49f89424cf94333e4c4df69850f2332
Shawn Lee <shawn.lee@calmid.com>
2019-11-12 16:32:20 +0900

    Added step 2; fixed onClick redux

--- client/src/pages/landing.tsx
+++ client/src/pages/landing.tsx
@@ -1,6 +1,7 @@
 import Button from "@material-ui/core/Button";
 import Chip from "@material-ui/core/Chip";
 import Container from "@material-ui/core/Container";
+import Slide from "@material-ui/core/Slide";
 import { createStyles, makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
@@ -16,6 +17,9 @@ import { useHistory } from "react-router";
 import BadanamuButton from "../components/button";
 import { ActionTypes } from "../store/actions";
 import { Store } from "../store/store";
+import TabletIcon from '@material-ui/icons/Tablet';
+import DevicesIcon from '@material-ui/icons/Devices';
+import LaptopIcon from '@material-ui/icons/Laptop';
 
 // tslint:disable:object-literal-sort-keys
 const useStyles = makeStyles((theme) => createStyles({
@@ -35,15 +39,11 @@ function createData(name: string, blp: Plan, blpPlus: Plan) {
     return { name, blp, blpPlus };
 }
 
-function continueWithPlan(selectedPlan: string) {
+export function Landing() {
     const store = useStore();
     const history = useHistory();
-    store.dispatch({ type: ActionTypes.PRODUCT_ID, payload: selectedPlan });
-    history.push("/login");
-}
-
-export function Landing() {
-    const classes = useStyles();
+    const classes = useStyles();;
+    const [step, setStep] = useState(1);
     const [selectedPlan, setPlan] = useState("blp");
     const rows = [
         createData("Bada Rhyme 1", <CheckRoundedIcon />, <CheckRoundedIcon />),
@@ -62,44 +62,69 @@ export function Landing() {
 
     return (
         <Container component="main" maxWidth="lg">
-            <div className={classes.paper}>
-                <Typography>STEP <b>1</b> OF <b>3</b></Typography>
-                <Typography variant="h4">Choose the pass that's right for you</Typography>
-                <Typography>Passes are valid for one year and billed annually</Typography>
-            </div>
-            <div className={classes.paper}>
-                <Table aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell></TableCell>
-                            <TableCell align="center">
-                                <Button value="blp" onClick={(e) => setPlan("blp")}>BLP</Button>
-                            </TableCell>
-                            <TableCell align="center">
-                                <Button value="blpPlus" onClick={(e) => setPlan("blp+")}>BLP+</Button>
-                            </TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows.map((row) => (
-                            <TableRow key={row.name}>
-                                <TableCell component="th" scope="row">
-                                    <Typography variant="subtitle1">{row.name}</Typography>
-                                </TableCell>
-                                <TableCell align="center">
-                                    {row.blp}
-                                </TableCell>
-                                <TableCell align="center">
-                                    {row.blpPlus}
-                                </TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </div>
-            <BadanamuButton fullWidth size="large" onClick={(e) => { continueWithPlan(selectedPlan); }}>
-                Continue with {selectedPlan}
-            </BadanamuButton>
+            <Slide direction="left" in={step === 1} timeout={{ exit: 10 }} mountOnEnter unmountOnExit>
+                <Container>
+                    <div className={classes.paper}>
+                        <Typography>STEP <b>{step}</b> OF <b>3</b></Typography>
+                        <Typography variant="h4">Choose the pass that's right for you</Typography>
+                        <Typography>Passes are valid for one year and billed annually</Typography>
+                    </div>
+                    <div className={classes.paper}>
+                        <Table aria-label="simple table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell></TableCell>
+                                    <TableCell align="center">
+                                        <Button value="blp" onClick={(e) => setPlan("blp")}>BLP</Button>
+                                    </TableCell>
+                                    <TableCell align="center">
+                                        <Button value="blpPlus" onClick={(e) => setPlan("blp+")}>BLP+</Button>
+                                    </TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {rows.map((row) => (
+                                    <TableRow key={row.name}>
+                                        <TableCell component="th" scope="row">
+                                            <Typography variant="subtitle1">{row.name}</Typography>
+                                        </TableCell>
+                                        <TableCell align="center">
+                                            {row.blp}
+                                        </TableCell>
+                                        <TableCell align="center">
+                                            {row.blpPlus}
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </div>
+                    <BadanamuButton fullWidth size="large" onClick={(e) => setStep(step + 1)}>
+                        Continue with {selectedPlan}
+                    </BadanamuButton>
+                </Container>
+            </Slide>
+            <Slide direction="left" in={step === 2} timeout={{ enter: 250, exit: 1000 }} mountOnEnter unmountOnExit>
+                <Container maxWidth="xs">
+                    <div className={classes.paper}>
+                        <Typography align="center">STEP <b>{step}</b> OF <b>3</b></Typography>
+                    </div>
+                    <div className={classes.paper}>
+                        <span align="center">
+                            <TabletIcon style={{ fontSize: "4em", marginBottom: 4 }} />
+                            <DevicesIcon style={{ fontSize: "6em" }} />
+                        </span>
+                        <Typography align="center" variant="h4">Create your account.</Typography>
+                        <Typography align="center">Use your email and create a password to access your KidsApps on any device at any time.</Typography>
+                    </div>
+                    <BadanamuButton fullWidth size="large" onClick={(e) => {
+                        store.dispatch({ type: ActionTypes.PRODUCT_ID, payload: selectedPlan });
+                        history.push("/signup");
+                    }}>
+                        Continue with {selectedPlan}
+                    </BadanamuButton>
+                </Container>
+            </Slide>
         </Container>
     );
 }
