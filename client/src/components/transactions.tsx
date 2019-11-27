fde3ae6d05f0f7983e99cd75e13de5fcbe686357
owen-delahoy <owen.delahoy@calmid.com>
2019-11-27 19:15:33 +0900

    Adapt to backend bug

--- client/src/components/transactions.tsx
+++ client/src/components/transactions.tsx
@@ -1,3 +1,4 @@
+import Paper from "@material-ui/core/Paper";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
@@ -9,15 +10,14 @@ import React from "react";
 import { FormattedDate } from "react-intl";
 import { useSelector } from "react-redux";
 import { State } from "../store/store";
-import Paper from "@material-ui/core/Paper";
 
 // TODO: Get pass information from DB later
 function PassIDName(id: string) {
     switch (id) {
         case "com.calmid.learnandplay.blp.standard":
-            return "Standard Pass";
+            return "Learning Pass";
         case "com.calmid.learnandplay.blp.premium":
-            return "Premium Pass";
+            return "Learning Pass Premium";
         default:
             return id;
     }
@@ -33,7 +33,7 @@ function Transaction(props: { transaction: any }) {
     const date = new Date(createdTimestamp).toLocaleString();
     const products = (props.transaction.ProductList as any[]).map((product: any) => JSON.stringify(product));
     const expiration = useSelector((state: State) => state.account.expireDate);
-    const passes = (props.transaction.PassList as any[]).filter((pass) => pass !== null).map((pass: any) => {
+    const passes = (props.transaction.PassList as any[]).map((pass: any) => {
         // return JSON.stringify(pass);
         return {
             end: pass.ExpirationDate as number,
@@ -46,13 +46,13 @@ function Transaction(props: { transaction: any }) {
         <React.Fragment>
             {
                 passes.map((pass) => (!pass ? null :
-                    <React.Fragment>
+                    <TableRow key={`${pass.start}-${pass.passId}`}>
                         <TableCell>{date}</TableCell>
-                        <TableCell>{pass.passName === "Standard Pass" ? "Learning Pass" : "Learning Pass Premium"}</TableCell>
+                        <TableCell>{pass.passName}</TableCell>
                         <TableCell>{date.split(",")[0]} - <FormattedDate value={expiration} /></TableCell>
                         <TableCell style={{ textTransform: "capitalize" }}>{store}</TableCell>
-                        <TableCell align="right">{pass.passName === "Standard Pass" ? "US$20" : "US$50"}</TableCell>
-                    </React.Fragment>
+                        <TableCell align="right">{pass.passName === "Learning Pass" ? "US$20" : "US$50"}</TableCell>
+                    </TableRow>
                 ))
             }
         </React.Fragment>
@@ -78,9 +78,7 @@ export function Transactions(props: Props) {
                 </TableHead>
                 {
                     props.transactions.map((transaction, i) => (
-                        <TableRow key={i}>
-                            <Transaction transaction={transaction} />
-                        </TableRow>
+                        <Transaction transaction={transaction} />
                     ))
                 }
             </Table>
