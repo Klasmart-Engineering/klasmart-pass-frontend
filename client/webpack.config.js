f825653ebb80c5d00ad91c742c1a5e9ea04a2230
owen-delahoy <owen.delahoy@calmid.com>
2019-11-28 15:09:40 +0900

    Webpack Production config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const Visualizer = require('webpack-visualizer-plugin');
 
+const output_file_name = 'bundle.[chunkhash].js'
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: ['babel-polyfill', './src/client-entry.tsx'],
     module: {
         rules: [
@@ -57,14 +59,40 @@ module.exports = {
         extensions: ['.js', '.jsx', '.tsx', '.ts'],
     },
     output: {
-        filename: 'bundle.js',
+        filename: output_file_name,
+        chunkFilename: output_file_name,
         path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: 'src/index.html',
         }),
+        new Visualizer({ filename: '../webpack-stats.html' }),
     ],
+    optimization: {
+        splitChunks: {
+            chunks: 'async',
+            minSize: 64 * 1024,
+            maxSize: 1024 * 1024,
+            minChunks: 1,
+            name: false,
+            maxAsyncRequests: 6,
+            maxInitialRequests: 4,
+            automaticNameDelimiter: '~',
+            automaticNameMaxLength: 30,
+            cacheGroups: {
+                vendors: {
+                    test: /[\\/]node_modules[\\/]/,
+                    priority: -10
+                },
+                default: {
+                    minChunks: 2,
+                    priority: -20,
+                    reuseExistingChunk: true
+                }
+            }
+        },
+    },
     devServer: {
         host: "0.0.0.0",
         proxy: {
