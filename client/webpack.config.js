594d145a2d02b20c259615eee9740366cebe0598
Adrien Lamoureux <adrien.lamoureux@calmid.com>
2020-02-04 13:38:55 +0900

    Fix compilation error by commenting whatwg-fetch

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -66,7 +66,7 @@ module.exports = {
             template: 'src/index.html',
         }),
         new webpack.ProvidePlugin({
-            'fetch': 'imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch'
+            //'fetch': 'imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch'
         }),
     ],
     devServer: {
