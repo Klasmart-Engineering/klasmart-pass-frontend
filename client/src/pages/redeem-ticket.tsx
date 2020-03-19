b1a8cc048ad513fbacaf4df898831a9f9e414eee
Adrien Lamoureux <adrien.lamoureux@calmid.com>
2020-03-19 13:33:46 +0900

    Fix ticket activation

--- client/src/pages/redeem-ticket.tsx
+++ client/src/pages/redeem-ticket.tsx
@@ -45,6 +45,7 @@ export function RedeemTicket(props: RouteComponentProps) {
     const restApi = useRestAPI();
 
     async function redeemTicket(e: React.FormEvent) {
+        e.preventDefault()
         if (inFlight) { return; }
         try {
             setInFlight(true);
