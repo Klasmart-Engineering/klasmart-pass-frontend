40d1a160eea970491fa78fc5da0642fd66cedd0d
Hoon <donghun.choi@calmisland.com>
2020-10-27 16:19:22 +0900

    https://calmisland.atlassian.net/browse/DSO-173

--- client/src/components/authorized.tsx
+++ client/src/components/authorized.tsx
@@ -1,57 +1,72 @@
-import React from "react"
-import { useSelector, useStore } from "react-redux";
-import { useHistory } from "react-router";
-import { ActionTypes } from "../store/actions";
-import { State } from "../store/store";
+import React from 'react';
+import { useSelector, useStore } from 'react-redux';
+import { useHistory } from 'react-router';
+import { ActionTypes } from '../store/actions';
+import { State } from '../store/store';
 
 export function isLoggedIn() {
-    const store = useStore();
-    const accessToken = useSelector((state: State) => state.account.accessToken);
-    const refreshToken = useSelector((state: State) => state.account.refreshToken);
-    return (accessToken === null && refreshToken === null);
+  const store = useStore();
+  const accessToken = useSelector((state: State) => state.account.accessToken);
+  const refreshToken = useSelector(
+    (state: State) => state.account.refreshToken
+  );
+  return accessToken === null && refreshToken === null;
 }
 
-export function redirectIfUnauthorized(returnRoute = "/") {
-    const store = useStore();
-    const history = useHistory();
-    const accessToken = useSelector((state: State) => state.account.accessToken);
-    const refreshToken = useSelector((state: State) => state.account.refreshToken);
-    const email = useSelector((state: State) => state.account.email);
+export function redirectIfUnauthorized(returnRoute = '/') {
+  const store = useStore();
+  const history = useHistory();
+  const accessToken = useSelector((state: State) => state.account.accessToken);
+  const refreshToken = useSelector(
+    (state: State) => state.account.refreshToken
+  );
+  const email = useSelector((state: State) => state.account.email);
 
+  React.useEffect(() => {
     if (accessToken === null && refreshToken === null) {
-        store.dispatch({ type: ActionTypes.POST_AUTHORIZATION_ROUTE, payload: returnRoute });
-        if (!email) {
-            history.replace("/signup");
-        } else {
-            history.replace("/login");
-        }
+      store.dispatch({
+        type: ActionTypes.POST_AUTHORIZATION_ROUTE,
+        payload: returnRoute,
+      });
+      if (!email) {
+        history.replace('/signup');
+      } else {
+        history.replace('/login');
+      }
     }
+  }, [accessToken, refreshToken]);
 }
 
-export function redirectIfAuthorized(defaultRoute = "/") {
-    const history = useHistory();
-    const accessToken = useSelector((state: State) => state.account.accessToken);
-    const refreshToken = useSelector((state: State) => state.account.refreshToken);
-    const postAuthorizationRoute = useSelector((state: State) => state.postAuthorizationRoute);
+export function redirectIfAuthorized(defaultRoute = '/') {
+  const history = useHistory();
+  const accessToken = useSelector((state: State) => state.account.accessToken);
+  const refreshToken = useSelector(
+    (state: State) => state.account.refreshToken
+  );
+  const postAuthorizationRoute = useSelector(
+    (state: State) => state.postAuthorizationRoute
+  );
 
-    React.useEffect(() => {
-        if (accessToken !== null || refreshToken !== null) {
-            history.replace(postAuthorizationRoute || defaultRoute);
-        }
-        
-    }, [accessToken, refreshToken])
-
+  React.useEffect(() => {
+    if (accessToken !== null || refreshToken !== null) {
+      history.replace(postAuthorizationRoute || defaultRoute);
+    }
+  }, [accessToken, refreshToken]);
 }
 
-export function redirectIfUnverifiable(defaultRoute = "/") {
-    const store = useStore();
-    const history = useHistory();
-    const accountId = useSelector((state: State) => state.account.accountId);
-    const accessToken = useSelector((state: State) => state.account.accessToken);
-    const refreshToken = useSelector((state: State) => state.account.refreshToken);
-    const postAuthorizationRoute = useSelector((state: State) => state.postAuthorizationRoute);
+export function redirectIfUnverifiable(defaultRoute = '/') {
+  const store = useStore();
+  const history = useHistory();
+  const accountId = useSelector((state: State) => state.account.accountId);
+  const accessToken = useSelector((state: State) => state.account.accessToken);
+  const refreshToken = useSelector(
+    (state: State) => state.account.refreshToken
+  );
+  const postAuthorizationRoute = useSelector(
+    (state: State) => state.postAuthorizationRoute
+  );
 
-    if ((accessToken !== null || refreshToken !== null) || accountId === null) {
-        history.replace(postAuthorizationRoute || defaultRoute);
-    }
+  if (accessToken !== null || refreshToken !== null || accountId === null) {
+    history.replace(postAuthorizationRoute || defaultRoute);
+  }
 }
