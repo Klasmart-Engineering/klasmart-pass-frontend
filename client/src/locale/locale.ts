f9a5d17fa1ed35c109b7f6598bcb2806912073dc
Henrik Wassdahl <henrik.wassdahl@calmid.com>
2021-10-20 17:31:58 +0900

    fix: add thai to loadable languages

--- client/src/locale/locale.ts
+++ client/src/locale/locale.ts
@@ -3,16 +3,19 @@ import { createIntl, createIntlCache } from "react-intl";
 import english from "./en.json";
 import indonesian from "./id.json";
 import korean from "./ko.json";
+import thai from "./th.json";
 
 import brandedEnglish from "@branding/locale/en.json";
 import brandedIndonesian from "@branding/locale/id.json";
 import brandedKorean from "@branding/locale/ko.json";
+import brandedThai from "@branding/locale/th.json";
 
 Object.assign(english, brandedEnglish);
 Object.assign(indonesian, brandedIndonesian);
 Object.assign(korean, brandedKorean);
+Object.assign(thai, brandedThai);
 
-export const localeCodes = ["en", "ko", "id"];
+export const localeCodes = ["en", "ko", "id", "th"];
 const intlCache = createIntlCache();
 export const fallbackLocale = createIntl(
   { locale: "en", messages: english },
@@ -29,5 +32,7 @@ export function getIntl(locale: string) {
       return createIntl({ locale: "ko", messages: korean }, intlCache);
     case "en":
       return createIntl({ locale: "en", messages: english }, intlCache);
+    case "th":
+      return createIntl({ locale: "th", messages: thai }, intlCache);
   }
 }
